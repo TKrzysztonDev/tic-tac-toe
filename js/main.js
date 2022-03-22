7a68940a7c9dc7610145d270c2d7a7e333437b1f
TomaszKrzyszton <tomasz.krzyszton.dev@gmail.com>
2022-03-22 18:40:28 +0100

    HTML structure and CSS styling

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let origBoard;
+const cells = document.querySelectorAll(".cell");
+const startGame = function () {
+  document.querySelector(".endgame").style.display = "none";
+  origBoard = Array.from(Array(9).keys());
+  cells.forEach((cell) => (cell.innerHTML = ""));
+};
+
+const huPlayer = "0";
+const aiPlayer = `X`;
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+
+startGame();
